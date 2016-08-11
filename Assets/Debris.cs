56efce243bd0903ea7dd042e1178b06039987d69
jihwankim <jihwan.kim@trustus.co.kr>
2016-08-11 14:26:15 +0900

    @ debris connected logic change

--- Assets/Debris.cs
+++ Assets/Debris.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class Debris : MonoBehaviour {
     bool isConnected = false;
 
+	void Update()
+	{
+		if (isConnected) {
+			//GetComponent<Rigidbody> ().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody>().velocity, 1.0f);
+		}
+	}
     // TODO: 어떻게 데브리와 플레이어를 결합시킬 것인가? (fixed joint는 연산도 많고 버그도 많음..)
     void OnCollisionEnter(Collision collision)
     {
@@ -15,7 +21,9 @@ public class Debris : MonoBehaviour {
             //transform.SetParent(collision.gameObject.transform);
             //GetComponent<FixedJoint>().connectedBody = collision.gameObject.GetComponent<Rigidbody>();
             transform.SetParent(GameObject.Find("Player").transform);
-            GetComponent<FixedJoint>().connectedBody = GameObject.Find("Player").GetComponent<Rigidbody>();
+			GetComponent<FixedJoint>().connectedBody = GameObject.Find("Player").GetComponent<Rigidbody>();
+			//GetComponent<Rigidbody>().isKinematic = true;
+			//GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
         }
     }
 }
